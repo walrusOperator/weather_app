9b8ad702e0078001ab028c1665f982c133ea59ee
Thomas Mullins <85575321+walrusOperator@users.noreply.github.com>
2023-07-19 15:17:09 -0500

    Restored ForecastScreen and Mainactivity after accidental deletion

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.weather_app
-
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -24,35 +22,68 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weather_app.ui.theme.Weather_appTheme
 import androidx.compose.foundation.Image
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weather_app.data.CurrentConditions
+import com.example.weather_app.data.Forecast
+
+private val forecastItems = listOf(
+    CurrentConditions(1675152000,1675152000, 1675198800, Forecast(72F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675238400,1675238400, 1675285200, Forecast(65F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675324800,1675324800, 1675371600, Forecast(45F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675411200,1675411200, 1675458000, Forecast(76F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675497600,1675497600, 1675544400, Forecast(77F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675584000,1675584000, 1675544400, Forecast(66F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675670400,1675670400, 1675717200, Forecast(54F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675756800,1675756800, 1675803600, Forecast(54F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675843200,1675843200, 1675890000, Forecast(80F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1675929600,1675929600, 1675976400, Forecast(72F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1676016000,1676016000, 1676062800, Forecast(75F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1676102400,1676102400, 1676149200, Forecast(32F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1676188800,1676188800, 1676235600, Forecast(85F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1676275200,1676275200, 1676322000, Forecast(72F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1676361600,1676361600, 1676408400, Forecast(72F, 65F, 80F), 1023F, 100),
+    CurrentConditions(1676448000,1676448000, 1676494800, Forecast(72F, 65F, 80F), 1023F, 100),
+)
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Weather_appTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    MyCurrentWeather()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "Start") {
+                    composable(route = "Start") {
+                        MyCurrentWeather(navController)
+                    }
+                    composable("ForecastScreen") {
+                        forecastItemList(dataItems = forecastItems, navController = navController)
+                    }
                 }
             }
         }
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MyCurrentWeather() {
+fun MyCurrentWeather(navController : NavController) {
+    TopBar()
     Column{
-        TopBar()
-        ShowWeather()
+        ShowWeather(navController = navController)
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBar() {
@@ -62,8 +93,10 @@ fun TopBar() {
         colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Blue)
     )
 }
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun ShowWeather() {
+fun ShowWeather(navController : NavController) {
+    Spacer(modifier = Modifier.height(60.dp))
     Column() {
         Column(
             modifier = Modifier
@@ -72,10 +105,10 @@ fun ShowWeather() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = stringResource(id = R.string.city),
-                fontSize = 23.sp,
-                fontWeight = FontWeight(480)
+                fontSize = 22.sp,
+                fontWeight = FontWeight(500)
             )
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(2.dp))
             Row(
                 modifier = Modifier.padding(horizontal = 30.dp),
                 verticalAlignment = Alignment.CenterVertically
@@ -118,27 +151,17 @@ fun ShowWeather() {
                 Text(text = stringResource(id = R.string.high), style = textStyle)
                 Text(text = stringResource(id = R.string.humidity), style = textStyle)
                 Text(text = stringResource(id = R.string.pressure), style = textStyle)
-
+            }
+            Spacer(modifier = Modifier.height(65.dp))
+            Button(onClick = {navController.navigate("ForecastScreen")},
+                shape = RectangleShape,
+                colors = ButtonDefaults.buttonColors(Color.Blue))
+            {
+                Text(text = "Forecast",
+                    fontSize = 18.sp,
+                    color = Color.White)
             }
         }
     }
 }
 
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Weather_appTheme {
-        Greeting("Android")
-    }
-}
